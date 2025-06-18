0d86225247fdfc7ee0c4e5f4ccf5b62207f7b265
Dev-Henry <96271705+Dev-Henry@users.noreply.github.com>
2025-06-18 11:54:46 +0000

    added the structure for the TicketComment

---
+++ Models/TicketComment.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vigilante.Models
+{
+    public class TicketComment
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Member Comment")]
+        public string Comment { get; set; }
+
+        [DisplayName("Date Created")]
+        public DateTimeOffset Created { get; set; }
+
+        [DisplayName("Ticket")]
+        public int TicketId { get; set; }
+
+        [DisplayName("Team Member")]
+        public string UserId { get; set; }
+
+
+
+        //Navigation properties
+        public virtual Ticket Ticket { get; set; }
+
+        public virtual VGUser User { get; set; }
+    }
+}
+
