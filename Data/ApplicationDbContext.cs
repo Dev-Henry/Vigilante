d502155985f25e533a864a02b5d53d8f306edd60
Dev-Henry <96271705+Dev-Henry@users.noreply.github.com>
2025-06-18 10:25:22 +0000

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Vigilante.Models;
+
+namespace Vigilante.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<VGUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
