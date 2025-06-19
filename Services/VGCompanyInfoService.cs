e6bae20ed37f5b9d75dfcfef075064ea943f32dc
Dev-Henry <96271705+Dev-Henry@users.noreply.github.com>
2025-06-19 16:02:02 +0000

    set up the constructor and added code to the methods in the class

---
+++ Services/VGCompanyInfoService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vigilante.Data;
+using Vigilante.Models;
+using Vigilante.Services.Interfaces;
+
+namespace Vigilante.Services
+{
+    public class VGCompanyInfoService : IVGCompanyInfoService
+    {
+        private readonly ApplicationDbContext _context; 
+
+        //setting up the constructor - dependency injection
+        public VGCompanyInfoService(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<List<VGUser>> GetAllMembersAsync(int companyId)
+        {
+            List<VGUser> result = new();
+            result = await _context.Users.Where(u => u.CompanyId == companyId).ToListAsync();
+            return result;
+        }
+
+        public async Task<List<Project>> GetAllProjectsAsync(int companyId)
+        {
+        }
+
+        public async Task<List<Ticket>> GetAllTicketAsync(int companyId)
+        {
+        }
+
+        public async Task<Company> GetCompanyInfoByIdAsync(int? companyId)
+        {
+        }
+    }
+}
