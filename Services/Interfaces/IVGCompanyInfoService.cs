61b5418585991dd9b5cbb16fcf0ea1f769afb576
Dev-Henry <96271705+Dev-Henry@users.noreply.github.com>
2025-06-19 15:49:51 +0000

    interface for CompanyInfo

---
+++ Services/Interfaces/IVGCompanyInfoService.cs
@@ -0,0 +1,16 @@
+﻿using Vigilante.Models;
+
+namespace Vigilante.Services.Interfaces
+{
+    public interface IVGCompanyInfoService
+    {
+        public Task<Company> GetCompanyInfoByIdAsync (int? companyId);
+
+        public Task<List<VGUser>> GetAllMembersAsync(int companyId);
+
+        public Task<List<Project>> GetAllProjectsAsync(int companyId);
+
+        public Task<List<Ticket>> GetAllTicketAsync(int companyId);
+
+    }
+}
