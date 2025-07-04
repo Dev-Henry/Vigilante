050812cbe4e93d28909caa064c3e1003062e82cf
Dev-Henry <96271705+Dev-Henry@users.noreply.github.com>
2025-07-04 16:43:48 +0000

    added code for TicketAttachment, fixed up the structure and added images to represent the uploaded attachments, styled up decription a little

--- Models/TicketAttachment.cs
+++ Models/TicketAttachment.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Drawing;
@@ -17,33 +18,35 @@ namespace Vigilante.Models
         public DateTimeOffset Created { get; set; }
 
         [DisplayName("Team Member")]
-        public string UserId { get; set; }
+        public string? UserId { get; set; }
 
 
         //attachment properties
         [DisplayName("File Description")]
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
         [NotMapped]
         [DisplayName("Select a file")]
         [DataType(DataType.Upload)]
         [MaxFileSize(1024 * 1024)]
-        [AllowedExtensions(new string[] { ".jpg", ".png", ".doc", ".docx", ".xls", ".xlsx", ".pdf" })]
-        public IFormFile FormFile { get; set; }
+        [AllowedExtensions(new string[] { ".jpg", ".png", ".doc", ".docx", ".xls", ".xlsx", ".pdf", ".ppt", ".pptx", ".html" })]
+        public IFormFile? FormFile { get; set; }
 
         [DisplayName("File Name")]
-        public string FileName { get; set; }
+        public string? FileName { get; set; }
 
-        public byte[] FileData { get; set; }
+        //I might take this out
+        public byte[]? FileData { get; set; }
 
         [DisplayName("File Extension")]
-        public string FileContentType { get; set; }
+        public string? FileContentType { get; set; }
 
 
         //Navigation Properties
-        public virtual Ticket Ticket { get; set; }
-
-        public virtual VGUser User { get; set; }
+        [BindNever]
+        public virtual Ticket? Ticket { get; set; }
+        [BindNever]
+        public virtual VGUser? User { get; set; }
 
     }
 }
